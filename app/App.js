c90359bfdf70b5700efb98afc9f417b0721949ab
BPodczerwinski <bpodczerwinski@roundarchisobar.com>
2015-07-25 02:05:21 -0500

    - Starts wiring up FLUX   - store, actions, and cursors
    - Adds some test properties on state model

--- app/App.js
+++ app/App.js
@@ -5,21 +5,34 @@
  * @author B.Podczerwinski
  */
 let React = require('react'),
+    StateTree = require('./StateTree'),
+    Cursors = require('./AppCursors'),
     Styles = require('./scss/master.scss');
 
 let App = React.createClass({
+    mixins: [
+        StateTree.mixin,
+        React.addons.PureRenderMixin
+    ],
+    cursors: {
+        app: Cursors.app
+    },
     getDefaultProps: function() {
         return {
-            appTitle: "Team Ram-Rod",
-            appSubtitle: "Chicago Hackathon",
-            version: "1.0"
+            appTitle: StateTree.select(Cursors['app']).get('title'),
+            appSubtitle: StateTree.select(Cursors['app']).get('subTitle'),
+            version: StateTree.select(Cursors['app']).get('version')
         }
     },
     render () {
+        let versionNumber = this.props.version.toFixed(2);
         return (
             <section className="app-base">
-                <h1>ISOBAR HACKATHON</h1>
-                <p>HELLO WORLD!!!!!</p>
+                <h1>
+                    { this.props.appTitle }
+                    <sup>Version: { versionNumber }</sup>
+                </h1>
+                <h2>{ this.props.appSubtitle }</h2>
             </section>
         );
     }
