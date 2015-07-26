8d28736618c86b93257b27610012487a0a643603
michael lancaster <mlancaster@mlancaster-m1.local>
2015-07-25 20:50:00 -0500

    add canvas drawing API

--- public/js/canvas.custom.js
+++ public/js/canvas.custom.js
@@ -1,33 +1,89 @@
 // Canvas scope
 (function(window, undefined) {
     window.Draw = {
-        el: document.querySelector('draw'),
+        el: document.querySelector('#draw'),
         canvas: null,
         context: null,
+        prevPosX: 0,
+        prevPosY: 0,
+        currPosX: 0,
+        currPosY: 0,
+        isMousePressed: false,
+        strokeStyle: '#ffffff',
 
-        createCanvas: function() {
+        start: function() {
 
             // create canvas
             this.canvas = document.createElement('canvas');
-            this.canvas.width = this.canvas.clientWidth;
-            this.canvas.height = this.canvas.clientHeight;
+            this.canvas.width = window.screen.width;
+            this.canvas.height = window.screen.height;
 
             // setup canvas
             this.context = this.canvas.getContext('2d');
             this.context.lineWidth = 5;
-            this.context.strokeStyle = '#fff';
+            this.context.strokeStyle = this.strokeStyle;
 
-            //this.el.textContent = this.canvas;
+
+            this.el.appendChild(this.canvas);
         },
 
-        drawLine: function(x, y) {
-            this.context.beginPath();
-            this.context.lineTo(x, y);
-            this.context.stroke();
+        line: function(x, y, isDown) {
+
+            if (isDown) {
+                this.context.beginPath();
+                this.context.moveTo(this.prevPosX, this.prevPosY);
+                this.context.lineTo(x, y);
+                this.context.closePath();
+                this.context.stroke();
+            }
+
+            this.prevPosX = x;
+            this.prevPosY = y;
+        },
+
+        setColor: function(keyword) {
+            this.strokeStyle = keyword;
+        },
+
+        resetColor: function() {
+            this.strokeStyle = '#ffffff'
+        },
+
+        clear: function() {
+            this.context.setTransform(1, 0, 0, 1, 0, 0);
+            this.context.clearRect(0, 0, this.context.canvas.width, this.context.canvas.height);
+        },
+
+
+        // Add listener prototype/test with mouse drawing
+        addListeners: function() {
+            var that = this;
+
+            $(this.canvas).mousedown(function (e) {
+                console.log('called A');
+                this.isMousePressed = true;
+                that.line(e.pageX - $(this).offset().left, e.pageY - $(this).offset().top, false);
+            });
+
+            $(this.canvas).mousemove(function (e) {
+                console.log('called B');
+                if (this.isMousePressed) {
+                    that.line(e.pageX - $(this).offset().left, e.pageY - $(this).offset().top, true);
+                }
+            });
+
+            $(this.canvas).mouseup(function (e) {
+                this.isMousePressed = false;
+            });
+
+            $(this.canvas).mouseleave(function (e) {
+                this.isMousePressed = false;
+            });
         }
 
 
     };
 
-    window.Draw.createCanvas();
+    window.Draw.start();
+    window.Draw.addListeners();
 })(window);
